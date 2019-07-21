c3cf1da1e8dc6f67dc9efa2bbd41dd1c2b75c619
Bran David <bran.david96@yahoo.com>
2019-07-21 19:46:30 +0300

    create table for username and controler c_login and model modelLogin

--- application/controllers/c_login.php
+++ application/controllers/c_login.php
@@ -6,6 +6,43 @@ class C_login extends CI_Controller{
         
         $this->load->view('v_form_login');
     }
+    public function login(){
+	$this->load->library('form_validation');
+	$this->form_validation->set_rules('username','UserName','trim|required|callback_check_user_login');
+	$this->form_validation->set_rules('password','Password','trim|required|md5');
+	if($this->form_validation->run()==false){
+		$this->index();
+	}else{
+		$user_session=array(
+			'UserName'       =>$this->input->post('username'),
+			'Password'       =>$this->input->post('password'),
+			'is_logged_in'   => 1
+		);
+		$this->session->set_userdata($user_session);
+		redirect('LogareController/admin');
+	}
+}   
+   public function check_user_login(){
+   $username=$this->input->post('username');//se trec din formular datele
+   $userpassword=$this->input->post('password');
+   $this->load->model('LoginModel');//se incarca modelul
+   if($this->LoginModel->is_logged_in($username,$userpassword)){
+   	 return true;
+   }else{
+   	$this->form_validation->set_message('check_user_login','Incorrect Username and Password');
+   	return false;
+   }
+
+  }
+  public function logout(){
+  	$this->session->sess_destroy();
+  	redirect('LogareController/index');
+  }
+
+   public function admin(){
+		$this->load->view('admin');
+
+  }
     
     
     
---
+++ application/models/modelLogIn.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit("No direct script access allowed");
+class LoginModel extends CI_Model{
+
+  public function is_logged_in($username,$userpassword){
+
+  	$this->db->where('userName',$username);//verificare date din baza de date 
+  	$this->db->where('password',md5($userpassword));
+        $this->db->from('user');
+    $query=$this->db->get();
+    if($query->num_rows()==1){
+    	return true;
+    }else{
+    	return false; 
+    }
+  }
+}
