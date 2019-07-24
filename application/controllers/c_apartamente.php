131361198f59dc90ce92093d3991bf01ed188e60
Bran David <bran.david96@yahoo.com>
2019-07-24 23:32:55 +0300

    clear cod c-v_apartamente

--- application/controllers/c_apartamente.php
+++ application/controllers/c_apartamente.php
@@ -16,124 +16,15 @@ class C_apartamente extends CI_Controller {
                 */
 		$this->load->view('v_apartamente',['posts'=>$posts]);
 	}
-        public function  create(){
-            
-            $this->load->view('v_create');
-            
-        }
-         public function  update($id){
-             echo $id;
-            $this->load->model('queries');
-            $post=$this->queries->getSinglePost($id);
-            $this->load->view('v_update',['post'=>$post]);
-            
-        }
-        public function  save(){
-          //  $this->load->database();           
-      //$dataEx=$this->input->post();
-      //print_r($dataEx);
-      //unset($dataEx['submit']);
-          $config=[
-         'upload_path'=>'./assets/imagini/',
-         'allowed_types'=>'gif|png|jpg'
-          ];  
-         $image_up='';  
-         $this->load->library('upload',$config);
-         $this->form_validation->set_error_delimiters();
-         if($this->upload->do_upload('userfile')){
-            
-             $info=$this->upload->data();
 
-             $image_up=$info['raw_name'].$info['file_ext'];
-   
-         }else{
-             echo "nu sa salvat imaginea";
-             exit();
-             
-         }     
-      $data=array(        
-       'nume'=>$this->input->post('nume'),         
-       'telefon'=>$this->input->post('nrTelefon'),                 
-        'pret'=>$this->input->post('pret'),
-         'adresa'=>$this->input->post('adresa'),
-          'imagine'=>$image_up
-        /*'imagine'=>$this->input->post('imagine'),*/    ); 
-        
-          
-   
-         $this->load->model('queries');
-         if($this->queries->addPost($data)){
-             $this->session->set_flashdata('msg','Sa salvat cu succes!');
-         }else{
-             $this->session->set_flashdata('msg','Nu sa salvat!');
-             
-         }    
-         redirect('c_apartamente/index'); 
-                        
-        }
-        public function change($id){
-            $config=[
-         'upload_path'=>'./assets/imagini/',
-         'allowed_types'=>'gif|png|jpg'
-          ];  
-         $image_up='';  
-         $this->load->library('upload',$config);
-         $this->form_validation->set_error_delimiters();
-         if($this->upload->do_upload('userfile')){
-            
-             $info=$this->upload->data();
-
-             $image_up=$info['raw_name'].$info['file_ext'];
-   
-         }else{
-             echo "nu sa salvat imaginea";
-             exit();
-             
-         }
-            //date preluate din v_update.php                        
-            $data=array(
-                
-       'nume'=>$this->input->post('nume'),         
-       'telefon'=>$this->input->post('nrTelefon'),                 
-        'pret'=>$this->input->post('pret'),
-         'adresa'=>$this->input->post('adresa'), 
-        'imagine'=>$image_up,    );        
-         $this->load->model('queries');
-         //interogare pt update
-         if($this->queries->updatePost($data,$id)){
-             $this->session->set_flashdata('msg','Sa modificat cu succes!');
-         }else{
-             $this->session->set_flashdata('msg','Nu sa salvat!');
-             
-         }    
-         redirect('c_apartamente/index'); 
-            
-        }
         public function view($id){
- //$this->load->view('c_view');
- //echo $id;
-    $this->load->model('queries');
-    $post=$this->queries->getSinglePost($id);
-    //echo print_r($post);
-    $this->load->view('v_view',['post'=>$post]);
+        $this->load->model('queries');
+        $post=$this->queries->getSinglePost($id);
+        $this->load->view('v_view',['post'=>$post]);
 
         }
 
-public function delete($id){
-    echo $id;
-    $this->load->model('queries');
-    $post=$this->queries->deletePost($id);
-    
-    if($this->queries->deletePost($id)){
-       
-    $this->session->set_flashdata('msg','Sa sters cu succes!');
-    redirect('c_apartamente/index');
-    }else{
-    $this->session->set_flashdata('msg','Nu sa  sters !');
 
-        
-    }
-}
         
         
         
--- application/views/v_apartamente.php
+++ application/views/v_apartamente.php
@@ -1,33 +1,8 @@
 <?php include_once 'header.php';?>
       <h3>Apartamente de inchiriat</h3>
-       <?php if($msg=$this->session->flashdata('msg')):?>
-      <?php echo $msg;?>
-      <?php endif; ?>
-   <?php 
-   $atts = array(
-        'class'       =>'btn btn-primary',
-        'width'       => 800,
-        'height'      => 700,
-        'scrollbars'  => 'yes',
-        'status'      => 'yes',
-        'resizable'   => 'yes',
-        'screenx'     => 0,
-        'screeny'     => 0,
-        'window_name' => '_blank');
-   $atts2 = array(
-        'class'       =>'label label-primary',
-        'width'       => 600,
-        'height'      => 200,
-        'scrollbars'  => 'yes',
-        'status'      => 'yes',
-        'resizable'   => 'yes',
-        'screenx'     => 0,
-        'screeny'     => 0,
-        'window_name' => '_blank');
-
-echo anchor('c_apartamente/create', 'inserare', $atts);
+    
    
-   ?>
+   
       
  <table class="table">
     <thead class="thead-light">
@@ -37,7 +12,6 @@ echo anchor('c_apartamente/create', 'inserare', $atts);
         <th>Pret</th>
         <th>Adresa</th>
         <th>Imagine</th>
-        <th>Action</th>
       </tr>
     </thead>
     <tbody>
@@ -49,14 +23,7 @@ echo anchor('c_apartamente/create', 'inserare', $atts);
         <td><?php echo $post->pret;?></td>
         <td><?php echo $post->adresa;?></td>
         <td><img width="120" src="<?php echo base_url().'/assets/imagini/'.$post->imagine;?>"></td>
-        <td>
-            <?php 
-            echo anchor("c_apartamente/view/{$post->id}", 'view', $atts);
-            echo anchor("c_apartamente/update/{$post->id}", 'edit', $atts);
-            echo anchor("c_apartamente/delete/{$post->id}", 'delete', $atts);
-            
-            ?>
-        </td>
+
       </tr>
       <?php endforeach; ?>
      <?php else: ?>
